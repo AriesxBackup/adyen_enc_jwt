0f06f6dfb73ff2a48205686573ebd59c25d440bc
r0ld3x <r0ld3xrahu1@gmail.com>
2023-12-31 23:19:53 +0530

    fixed express app

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const encryptCardData = require('../src/encrypt');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const encryptCardData = require("./src/encrypt");
 
+app.get("/", async (req, res) => {
+  const cc = req.query.cc;
+  const mes = req.query.mes;
+  const ano = req.query.ano;
+  const cvv = req.query.cvv;
+  const context = req.query.context;
+  const x = await encryptCardData(cc, mes, ano, cvv, context);
+  res.send(x);
+});
 
-
-app.get('/', async (req, res) => {
-    //res.send('Hello World!')
-    const cc = req.query.cc
-    const mes = req.query.mes
-    const ano = req.query.ano
-    const cvv = req.query.cvv
-    const context = req.query.context
-    const x = await encryptCardData(
-        card, month, year, cvc, adyenKey
-    )
-    console.log(x)
-    res.send(x)
-})
-
-const card = cc;
-const month = mes;
-const year = ano;
-const cvc = cvv;
-
-const adyenKey = context;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
